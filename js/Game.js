6ec09f3648f520aeeb0e68359d3b742134c8e804
moltra <strickland41@live.marshall.edu>
2017-01-26 02:02:44 -0500

    top of page 14

--- js/Game.js
+++ js/Game.js
@@ -5,9 +5,13 @@ SpaceHipster.Game = function () {};
 
 SpaceHipster.Game.prototype = {
   create: function() {
+
     //set world dimensions
     this.game.world.setBounds(0, 0, 1920, 1920);
     this.background=this.game.add.tileSprite(0, 0, this.game.world.width, this.game.world.height, 'space');
+
+    //create player
+    
   },
   update: function() {
   },
